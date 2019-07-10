940c8a86e587fe2631bcdf8395830ee805c979dc
ChristineWasike <christinewasike22@gmail.com>
2019-07-10 11:19:42 +0300

    Working on activity that displays details about a single producted when clicked

--- app/src/main/java/com/spongey/ecommerce/ProductDetails.kt
+++ app/src/main/java/com/spongey/ecommerce/ProductDetails.kt
@@ -2,11 +2,15 @@ package com.spongey.ecommerce
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_product_details.*
 
 class ProductDetails : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_details)
+        val productTitle = intent.getStringExtra("title")
+
+        productDetailsTitle.text = productTitle
     }
 }
