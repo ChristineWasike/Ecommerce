e5631b8cba67823c0b5b1e5bb15c1e600cfba50b
ChristineWasike <christinewasike22@gmail.com>
2019-07-10 10:07:14 +0300

    Extracted a new string

---
+++ app/src/main/java/com/spongey/ecommerce/ProductDetails.kt
@@ -0,0 +1,12 @@
+package com.spongey.ecommerce
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProductDetails : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_details)
+    }
+}
