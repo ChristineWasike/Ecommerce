8403ef4f71dbfef4ad9be698fa4fd49575d6916b
ChristineWasike <christinewasike22@gmail.com>
2019-07-10 15:58:12 +0300

    Added in functionality to the availability button and brings up an alert dialog indication product availability

--- app/src/main/java/com/spongey/ecommerce/ProductDetails.kt
+++ app/src/main/java/com/spongey/ecommerce/ProductDetails.kt
@@ -2,15 +2,28 @@ package com.spongey.ecommerce
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_product_details.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 
 class ProductDetails : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_details)
+        setSupportActionBar(toolbar)
         val productTitle = intent.getStringExtra("title")
 
         productDetailsTitle.text = productTitle
+
+        availabilityButton.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setMessage("Hey $productTitle is in stock")
+                .setPositiveButton("OK") { p0, p1 ->
+
+                }
+                .create()
+                .show()
+        }
     }
 }
