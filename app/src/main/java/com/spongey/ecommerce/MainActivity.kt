c175c969852fd6abc61b3bb4feb52998018b20dd
ChristineWasike <christinewasike22@gmail.com>
2019-07-10 15:57:29 +0300

    Refactored code to prevent redundancy

--- app/src/main/java/com/spongey/ecommerce/MainActivity.kt
+++ app/src/main/java/com/spongey/ecommerce/MainActivity.kt
@@ -2,29 +2,71 @@ package com.spongey.ecommerce
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.Window
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.navigation.NavigationView
 import com.spongey.ecommerce.model.Product
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.navigation_drawer.*
 
-class MainActivity : AppCompatActivity() {
-    val apiKey = BuildConfig.Api_Key
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.navigation_drawer)
+        setSupportActionBar(toolbar)
 
-    val products = arrayListOf<Product>()
+
+        val toggle = ActionBarDrawerToggle(this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+
+
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+        navigationView.setNavigationItemSelectedListener(this)
+
+        val products = arrayListOf<Product>()
 
         for (i in 1..100) {
-            products.add(Product("Organic Apple  #$i", "http://www.pngmart.com/files/5/Red-Apple-PNG-Photos.png", 200.00))
+            products.add(
+                Product(
+                    "Organic Apple  #$i",
+                    "http://www.pngmart.com/files/5/Red-Apple-PNG-Photos.png",
+                    200.00
+                )
+            )
         }
         recyclerView.apply {
             layoutManager = GridLayoutManager(this@MainActivity, 2)
             adapter = ProductsAdapter(products)
         }
 
-        Toast.makeText(this, apiKey, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onBackPressed() {
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
