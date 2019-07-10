53c2aa99fae819587b00836d71aa558037061981
ChristineWasike <christinewasike22@gmail.com>
2019-07-10 11:22:50 +0300

    Setting up the onclick listener for a singleitem and setting up an intent to its details activity

--- app/src/main/java/com/spongey/ecommerce/ProductsAdapter.kt
+++ app/src/main/java/com/spongey/ecommerce/ProductsAdapter.kt
@@ -1,2 +1,43 @@
 package com.spongey.ecommerce
 
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.spongey.ecommerce.ProductsAdapter.*
+import com.spongey.ecommerce.model.Product
+import com.squareup.picasso.Picasso
+
+class ProductsAdapter(private val products: ArrayList<Product>) : RecyclerView.Adapter<ViewHolder>() {
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val product = products[position]
+        Picasso.get().load(product.photoUrl).into(holder.image)
+        holder.title.text = product.title
+        holder.price.text = product.price.toString()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.product_row, parent, false)
+        val holder = ViewHolder(view)
+        view.setOnClickListener {
+            val intent = Intent(parent.context, ProductDetails::class.java)
+            intent.putExtra("title", products[holder.adapterPosition].title)
+            parent.context.startActivity(intent)
+        }
+        return holder
+    }
+
+    override fun getItemCount() = products.size
+
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val image: ImageView = itemView.findViewById(R.id.appleImage)
+        val title: TextView = itemView.findViewById(R.id.productTitle)
+        val price: TextView = itemView.findViewById(R.id.priceTag)
+    }
+}
