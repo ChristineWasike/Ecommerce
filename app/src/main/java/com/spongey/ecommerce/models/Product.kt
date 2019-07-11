8be327dd03090d8869b77a711ee09b0c6e795985
ChristineWasike <christinewasike22@gmail.com>
2019-07-11 12:09:53 +0300

    Changed property names to prevent mapping issue Serialization can also do the trick -> ASerializedName("name") rather than changing existing "title" in the model

--- app/src/main/java/com/spongey/ecommerce/models/Product.kt
+++ app/src/main/java/com/spongey/ecommerce/models/Product.kt
@@ -2,7 +2,7 @@ package com.spongey.ecommerce.models
 
 
 data class Product(
-    val title: String,
-    val photoUrl: String,
+    val name: String,
+    val image: String,
     val price: Double
 )
