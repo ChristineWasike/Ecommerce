76b66590cfad83db0f05a1331a65338715334be2
ChristineWasike <christinewasike22@gmail.com>
2019-07-11 11:59:16 +0300

    Parsing data from the generated JSON file and displaying the information

--- app/src/main/java/com/spongey/ecommerce/ui/fragments/MainFragment.kt
+++ app/src/main/java/com/spongey/ecommerce/ui/fragments/MainFragment.kt
@@ -1,14 +1,34 @@
 package com.spongey.ecommerce.ui.fragments
 
 import android.os.Bundle
+import android.util.Log.d
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import com.google.gson.Gson
 import com.spongey.ecommerce.R
+import com.spongey.ecommerce.adapters.ProductsAdapter
+import com.spongey.ecommerce.models.Product
+import kotlinx.android.synthetic.main.fragment_main.view.*
+import java.io.InputStream
+
 
 class MainFragment: Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        val root = inflater.inflate(R.layout.fragment_main, container, false)
+
+        val inputStream: InputStream = activity!!.assets.open("generated.json")
+        val inputString  =inputStream.bufferedReader().use { it.readText()}
+
+        val products = Gson().fromJson(inputString, Array<Product>::class.java).toList()
+
+        d("Data",  inputString)
+        root.recyclerView.apply {
+            layoutManager = GridLayoutManager(activity, 2)
+            adapter = ProductsAdapter(products)
+        }
+        return root
     }
 }
