17fe4ffb27c3f9cbce2c5d600445c49f7573c50d
ChristineWasike <christinewasike22@gmail.com>
2019-07-11 09:55:44 +0300

    Inflating the main fragment layout Facing a bug currently where the main_fragment resouce layout is unrecognized

--- app/src/main/java/com/spongey/ecommerce/ui/fragments/MainFragment.kt
+++ app/src/main/java/com/spongey/ecommerce/ui/fragments/MainFragment.kt
@@ -1,4 +1,14 @@
 package com.spongey.ecommerce.ui.fragments
 
-class MainFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.spongey.ecommerce.R
+
+class MainFragment: Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
 }
