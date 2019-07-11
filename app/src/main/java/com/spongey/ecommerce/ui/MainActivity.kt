27f6abb57bfa4073ec50392ac07dd76051994619
ChristineWasike <christinewasike22@gmail.com>
2019-07-11 09:58:26 +0300

    Populating the Main Activity with information on fragments through the framelayout Also workign on enabling selection of fragments from the navigation drawer

--- app/src/main/java/com/spongey/ecommerce/ui/MainActivity.kt
+++ app/src/main/java/com/spongey/ecommerce/ui/MainActivity.kt
@@ -1,20 +1,18 @@
-package com.spongey.ecommerce
+package com.spongey.ecommerce.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log.d
-import android.view.Menu
 import android.view.MenuItem
-import android.view.Window
-import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.navigation.NavigationView
-import com.spongey.ecommerce.model.Product
+import com.spongey.ecommerce.ui.fragments.ApplesFragment
+import com.spongey.ecommerce.adapters.ProductsAdapter
+import com.spongey.ecommerce.R
+import com.spongey.ecommerce.models.Product
+import com.spongey.ecommerce.ui.fragments.MainFragment
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 import kotlinx.android.synthetic.main.navigation_drawer.*
@@ -26,6 +24,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         setContentView(R.layout.navigation_drawer)
         setSupportActionBar(toolbar)
 
+        supportFragmentManager.beginTransaction().replace(R.id.frameLayout, MainFragment()).commit()
 
         val toggle = ActionBarDrawerToggle(
             this,
@@ -34,7 +33,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             R.string.navigation_drawer_open,
             R.string.navigation_drawer_close
         )
-
 
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
@@ -71,10 +69,10 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         when (item.itemId) {
-            R.id.actionApples -> {
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.frameLayout, ApplesFragment()).commit()
-            }
+//            R.id.actionApples -> {
+//                supportFragmentManager.beginTransaction()
+//                    .replace(R.id.frameLayout, ApplesFragment()).commit()
+//            }
 
         }
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
