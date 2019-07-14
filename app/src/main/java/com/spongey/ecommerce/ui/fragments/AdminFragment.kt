07627f24e297348dd7ac37ef354fe05c263a169d
ChristineWasike <christinewasike22@gmail.com>
2019-07-14 13:50:32 +0300

    Added functionality to the addProduct button such that the input product tile is stored to the local databse together with teh hardcoded image URL and price

--- app/src/main/java/com/spongey/ecommerce/ui/fragments/AdminFragment.kt
+++ app/src/main/java/com/spongey/ecommerce/ui/fragments/AdminFragment.kt
@@ -1,15 +1,45 @@
 package com.spongey.ecommerce.ui.fragments
 
 import android.os.Bundle
+import android.util.Log.d
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.room.Room
 import com.spongey.ecommerce.R
+import com.spongey.ecommerce.database.AppDatabase
+import com.spongey.ecommerce.database.CachedProduct
+import kotlinx.android.synthetic.main.fragment_admin.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
 
 class AdminFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_apples, container, false)
+        return inflater.inflate(R.layout.fragment_admin, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        addProduct.setOnClickListener {
+            val title = inputTitle.text
+            d("Clicked","You tried to add a $title")
+
+            doAsync {
+                //creating an instance of the database
+                val db = Room.databaseBuilder(
+                    activity!!.applicationContext, AppDatabase::class.java, "database-name"
+                ).build()
+
+                //caching the products into the database
+                db.productDao().insertAll(CachedProduct(null, title.toString(), 25.00))
+
+                val products = db.productDao().getAll()
+                uiThread {
+                    d("Adding ...", "$title added to the database")
+                }
+            }
+        }
     }
 
 
