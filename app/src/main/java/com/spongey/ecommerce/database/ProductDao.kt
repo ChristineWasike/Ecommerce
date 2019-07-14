ca3d8e92c2d235caf0c7fa39d31280d7fec0878e
ChristineWasike <christinewasike22@gmail.com>
2019-07-14 13:54:38 +0300

    Created an abstract Data Access Object to help store and retrieve products in the Room Databse

--- app/src/main/java/com/spongey/ecommerce/database/ProductDao.kt
+++ app/src/main/java/com/spongey/ecommerce/database/ProductDao.kt
@@ -1,4 +1,14 @@
 package com.spongey.ecommerce.database
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
 interface ProductDao {
+    @Query("SELECT * FROM cachedProduct")
+    fun getAll(): List<CachedProduct>
+
+   @Insert
+   fun insertAll(vararg products: CachedProduct)
 }
