57775fa31b5032d59b957aec9e29c573565f1713
ChristineWasike <christinewasike22@gmail.com>
2019-07-14 13:59:52 +0300

    Pulling the cateogry names direct from the product titles for now and displaying them at the top of the main activity

--- app/src/main/java/com/spongey/ecommerce/adapters/CategoriesAdapter.kt
+++ app/src/main/java/com/spongey/ecommerce/adapters/CategoriesAdapter.kt
@@ -1,4 +1,27 @@
 package com.spongey.ecommerce.adapters
 
-class CategoriesAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Adapter
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.spongey.ecommerce.R
+
+class CategoriesAdapter(val categories: List<String>) : RecyclerView.Adapter<CategoriesAdapter.ViewHolder>() {
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    holder.category.text = categories[position]
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.category_row, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount() = categories.size
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val category: TextView = itemView.findViewById(R.id.categoryName)
+    }
 }
