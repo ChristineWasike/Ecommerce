6fdcfaf69c26457bf10656ccd8140aae63461728
ChristineWasike <christinewasike22@gmail.com>
2019-07-14 13:56:28 +0300

    This is a 'model' illustrating the properties required to store a product into the Room Databse

--- app/src/main/java/com/spongey/ecommerce/database/CachedProduct.kt
+++ app/src/main/java/com/spongey/ecommerce/database/CachedProduct.kt
@@ -1,2 +1,14 @@
 package com.spongey.ecommerce.database
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class CachedProduct(
+    @PrimaryKey(autoGenerate = true) val pid: Int?,
+    @ColumnInfo val name: String,
+//    @ColumnInfo val image: String,
+    @ColumnInfo val price: Double
+//    @ColumnInfo val isOnSale: Boolean
+)
