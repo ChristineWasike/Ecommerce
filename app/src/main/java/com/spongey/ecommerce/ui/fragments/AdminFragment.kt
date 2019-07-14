8945bbc049d1800c8e6fa2f6f93d273727da01dc
ChristineWasike <christinewasike22@gmail.com>
2019-07-14 13:43:34 +0300

    Extracted new strings

---
+++ app/src/main/java/com/spongey/ecommerce/ui/fragments/AdminFragment.kt
@@ -0,0 +1,16 @@
+package com.spongey.ecommerce.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.spongey.ecommerce.R
+
+class AdminFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_apples, container, false)
+    }
+
+
+}
