eddc873ab7f4766bd707f19f81e04ee73963d4a8
ChristineWasike <christinewasike22@gmail.com>
2019-07-14 13:57:34 +0300

    Created an instance of the AppDatabse that will be created in the users device

--- app/src/main/java/com/spongey/ecommerce/database/AppDatabase.kt
+++ app/src/main/java/com/spongey/ecommerce/database/AppDatabase.kt
@@ -1,4 +1,10 @@
 package com.spongey.ecommerce.database
 
-class AppDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [CachedProduct::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract  fun productDao(): ProductDao
 }
