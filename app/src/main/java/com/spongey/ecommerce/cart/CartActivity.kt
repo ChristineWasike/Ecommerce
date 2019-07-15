dc4bbc3f9b63034995eea12709975de4608253e8
ChristineWasike <christinewasike22@gmail.com>
2019-07-15 16:16:24 +0300

    Created a Cart Activity

--- app/src/main/java/com/spongey/ecommerce/cart/CartActivity.kt
+++ app/src/main/java/com/spongey/ecommerce/cart/CartActivity.kt
@@ -1,4 +1,12 @@
 package com.spongey.ecommerce.cart
 
-class CartActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.spongey.ecommerce.R
+
+class CartActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart)
+    }
 }
