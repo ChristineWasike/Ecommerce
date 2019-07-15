7cb5103cd09fb8f6d6a08794c8ee8d987dc8d9c7
ChristineWasike <christinewasike22@gmail.com>
2019-07-15 16:14:13 +0300

    Created a ProductREpository that queries the json file for the product objects

--- app/src/main/java/com/spongey/ecommerce/repos/ProductsRepository.kt
+++ app/src/main/java/com/spongey/ecommerce/repos/ProductsRepository.kt
@@ -1,4 +1,26 @@
 package com.spongey.ecommerce.repos
 
+import android.content.Context
+import android.content.res.AssetManager
+import com.google.gson.Gson
+import com.google.gson.annotations.SerializedName
+import com.spongey.ecommerce.models.Product
+import com.spongey.ecommerce.ui.fragments.MainFragment
+import io.reactivex.Single
+import io.reactivex.Single.create
+import java.io.InputStream
+import java.net.URL
+
 class ProductsRepository {
+    private val context: Context? = null
+
+    fun getAllProducts(): Single<List<Product>> {
+
+        return create<List<Product>> {
+            val json = URL("https://api.myjson.com/bins/1gf1kz").readText()
+            val products = Gson().fromJson(json, Array<Product>::class.java).toList()
+            it.onSuccess(products)
+        }
+    }
+
 }
