5c71437a23b26bca8cec5068acacac7f2cca4f47
ChristineWasike <christinewasike22@gmail.com>
2019-07-15 16:15:49 +0300

    Removed dead code

--- app/src/main/java/com/spongey/ecommerce/repos/ProductsRepository.kt
+++ app/src/main/java/com/spongey/ecommerce/repos/ProductsRepository.kt
@@ -1,18 +1,12 @@
 package com.spongey.ecommerce.repos
 
-import android.content.Context
-import android.content.res.AssetManager
 import com.google.gson.Gson
-import com.google.gson.annotations.SerializedName
 import com.spongey.ecommerce.models.Product
-import com.spongey.ecommerce.ui.fragments.MainFragment
 import io.reactivex.Single
 import io.reactivex.Single.create
-import java.io.InputStream
 import java.net.URL
 
 class ProductsRepository {
-    private val context: Context? = null
 
     fun getAllProducts(): Single<List<Product>> {
 
