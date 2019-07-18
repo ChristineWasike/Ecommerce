65f4654b6a226088de93299ce65f2aa9e50e0541
ChristineWasike <christinewasike22@gmail.com>
2019-07-18 14:30:01 +0300

    Added functions to pull data from the db i.e. search and pull by product name

--- app/src/main/java/com/spongey/ecommerce/repos/ProductsRepository.kt
+++ app/src/main/java/com/spongey/ecommerce/repos/ProductsRepository.kt
@@ -10,11 +10,34 @@ class ProductsRepository {
 
     fun getAllProducts(): Single<List<Product>> {
 
-        return create<List<Product>> {
-            val json = URL("https://api.myjson.com/bins/1gf1kz").readText()
-            val products = Gson().fromJson(json, Array<Product>::class.java).toList()
-            it.onSuccess(products)
+        return create {
+            it.onSuccess(fetchProducts())
         }
+    }
+
+
+    fun searchForProducts(searchTerm: String): Single<List<Product>> {
+        return create {
+            val filterProducts = fetchProducts().filter {
+                it.name.contains(searchTerm, true)
+            }
+            it.onSuccess(filterProducts)
+        }
+    }
+
+    fun getProductByName(name: String): Single<Product>{
+        return create {
+            val product = fetchProducts().first {
+                it.name == name
+            }
+            it.onSuccess(product)
+        }
+    }
+
+    private fun fetchProducts(): List<Product> {
+        val json =
+            URL("https://gist.githubusercontent.com/ChristineWasike/409619a12abd3d616d6426281487b839/raw/d6b5e4489e18a16c8333cb19c20dfdec534521f3/fruit_product.json").readText()
+        return Gson().fromJson(json, Array<Product>::class.java).toList()
     }
 
 }
