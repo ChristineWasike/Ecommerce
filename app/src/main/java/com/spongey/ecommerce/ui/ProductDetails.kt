016655fd9bb214376a4391aefed22d2165ae0246
ChristineWasike <christinewasike22@gmail.com>
2019-07-18 14:29:05 +0300

    Refactored this activity to directly get data from the database rather than intent filters

--- app/src/main/java/com/spongey/ecommerce/ui/ProductDetails.kt
+++ app/src/main/java/com/spongey/ecommerce/ui/ProductDetails.kt
@@ -1,12 +1,18 @@
 package com.spongey.ecommerce.ui
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log.d
+import android.view.MenuItem
 import androidx.appcompat.app.AlertDialog
 import com.spongey.ecommerce.R
+import com.spongey.ecommerce.cart.CartActivity
+import com.spongey.ecommerce.repos.ProductsRepository
 import com.squareup.picasso.Picasso
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_product_details.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
@@ -16,41 +22,55 @@ class ProductDetails : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_details)
+
+        //UpButtonEnabled
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         val productTitle = intent.getStringExtra("title")
         val productImage = intent.getStringExtra("image")
         val productPrice = intent.getStringExtra("price")
 
-        productDetailsTitle.text = productTitle
-        Picasso.get().load(productImage).into(productDetailsImage)
-        productDetailsPrice.text = "Ksh. $productPrice"
+        val product = ProductsRepository().getProductByName(productTitle)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                productDetailsTitle.text = it.name
 
+                productDetailsPrice.text = "Ksh. ${it.price}"
+            }, {})
+
+
+
+        Picasso.get().load(productImage).into(productDetailsImage)
 
         availabilityButton.setOnClickListener {
 
-            if (productTitle == "Mango") {
-                AlertDialog.Builder(this)
+            when (productTitle) {
+                "Mango" -> AlertDialog.Builder(this)
                     .setMessage("Hey, ${productTitle}es are in stock.")
                     .setPositiveButton("OK") { p0, p1 ->
 
                     }
                     .create()
                     .show()
-            } else if (productTitle == "Grapes")
-                AlertDialog.Builder(this)
+                "Grapes" -> AlertDialog.Builder(this)
                     .setMessage("Hey, $productTitle are in stock.")
                     .setPositiveButton("OK") { p0, p1 ->
 
                     }
                     .create()
                     .show()
-            else
-                AlertDialog.Builder(this)
+                else -> AlertDialog.Builder(this)
                     .setMessage("Hey, ${productTitle}s are in stock.")
                     .setPositiveButton("OK") { p0, p1 ->
 
                     }
                     .create()
                     .show()
+            }
         }
+
     }
+
+    
 }
