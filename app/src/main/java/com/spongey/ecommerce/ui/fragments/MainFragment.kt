ab17bea4a3d26fc4f035adba089e311c0b45d80d
ChristineWasike <christinewasike22@gmail.com>
2019-07-18 14:27:07 +0300

    Done some code refactoring and restored the category recyclerView though with hardcoded data

--- app/src/main/java/com/spongey/ecommerce/ui/fragments/MainFragment.kt
+++ app/src/main/java/com/spongey/ecommerce/ui/fragments/MainFragment.kt
@@ -1,6 +1,6 @@
 package com.spongey.ecommerce.ui.fragments
 
-import android.annotation.SuppressLint
+
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log.d
@@ -13,27 +13,21 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.room.Room
-import com.google.gson.Gson
+
 import com.spongey.ecommerce.R
 import com.spongey.ecommerce.adapters.CategoriesAdapter
 import com.spongey.ecommerce.adapters.ProductsAdapter
-import com.spongey.ecommerce.database.AppDatabase
-import com.spongey.ecommerce.database.CachedProduct
 import com.spongey.ecommerce.models.Product
+
 import com.spongey.ecommerce.repos.ProductsRepository
 import com.spongey.ecommerce.ui.ProductDetails
-import io.reactivex.Scheduler
+import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_main.*
-import kotlinx.android.synthetic.main.fragment_main.view.*
+
 import kotlinx.android.synthetic.main.fragment_main.view.categoriesRecyclerView
-import kotlinx.android.synthetic.main.product_row.view.*
-import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.uiThread
-import java.io.InputStream
-import java.util.ArrayList
+import org.jetbrains.anko.sdk27.coroutines.onClick
 
 
 class MainFragment : Fragment() {
@@ -41,27 +35,12 @@ class MainFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_main, container, false)
 
 
-//        val inputStream: InputStream = activity!!.assets.open("generated.json")
-//        val inputString = inputStream.bufferedReader().use { it.readText() }
-//
-//
-//        val products = Gson().fromJson(inputString, Array<Product>::class.java).toList()
-//        root.recyclerView.apply {
-//            layoutManager = GridLayoutManager(activity, 2)
-//            adapter = ProductsAdapter(products)
-//            root.progressBar.visibility = View.GONE
-//            fruits = products
-//        }
-//
-//        val categories: ArrayList<String> = ArrayList()
-//        for (fruit in fruits)
-//            categories.add(fruit.name)
-//
-//
-//        root.categoriesRecyclerView.apply {
-//            layoutManager = LinearLayoutManager(activity, RecyclerView.HORIZONTAL, false)
-//            adapter = CategoriesAdapter(categories)
-//        }
+        val fruits = listOf("Citrus", "Berries", "Apples & Pears", "Tropical & Exotic", "Melons")
+
+        root.categoriesRecyclerView.apply {
+            layoutManager = LinearLayoutManager(activity, RecyclerView.HORIZONTAL, false)
+            adapter = CategoriesAdapter(fruits)
+        }
 
         return root
     }
@@ -69,10 +48,20 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        var fruits: List<Product>
+
 
         val productsRepository = ProductsRepository().getAllProducts()
-            .subscribeOn(Schedulers.io())
+        loadRecyclerView(productsRepository)
+
+        searchButton.onClick {
+            loadRecyclerView(ProductsRepository().searchForProducts(searchProduct.text.toString()))
+        }
+
+    }
+
+    private fun loadRecyclerView(productsRepository: Single<List<Product>>) {
+       val single= productsRepository
+        .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe({
                 d("Christine", "success:) ")
@@ -93,14 +82,10 @@ class MainFragment : Fragment() {
                         startActivity(intent, options.toBundle())
                     }
 
-
-                    fruits = it
                 }
                 progressBar.visibility = View.GONE
             }, {
                 d("Christine", "error:( ${it.message}")
             })
-
-
     }
 }
